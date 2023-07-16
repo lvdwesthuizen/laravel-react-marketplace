00c7cb981f6c7ec236ade435ce0e22d32c2a43ee
lauren_vanderwesthuizen <cubensista@gmail.com>
2023-07-16 17:20:02 +0200

    Small update to fix bug in login test

--- react/src/views/Login.test.jsx
+++ react/src/views/Login.test.jsx
@@ -1,21 +1,34 @@
 import { describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
 import Login from "./Login";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Login page renders", () => {
   it("renders the Login page", () => {
-    const { getByText } = render(<Login />);
+    const { getByText } = render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
     expect(getByText(/log in/i)).toBeInTheDocument();
   });
 
   it("renders two input components", () => {
-    const { getByLabelText } = render(<Login />);
+    const { getByLabelText } = render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
     expect(getByLabelText(/email/i)).toBeInTheDocument();
     expect(getByLabelText(/password/i)).toBeInTheDocument();
   });
 
   it("renders a submit button", () => {
-    const { getByText } = render(<Login />);
+    const { getByText } = render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
     expect(getByText("Submit")).toBeInTheDocument();
   });
 });
